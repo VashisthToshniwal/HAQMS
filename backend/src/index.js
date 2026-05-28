87822f543e98f21edf1f58dd8e967bf16fd8b555
Vashisthlnmit <vashisthkumar54@gmail.com>
2026-05-28 18:43:25 +0530

    api rate limiter issue fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
 
 // Load environment variables
 dotenv.config();
